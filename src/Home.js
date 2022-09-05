ff69f7407a5f7c0aca2c2ab03454624653280ce4
bevannutakor <bevannutakor@icloud.com>
2022-09-05 02:48:19 -0700

    bug fix

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import computer from './Assets/computer_explorer.png';
 import search from './Assets/search_directory.png';
 import folder from './Assets/directory_closed_cool.png';
 
-import ChatRooms from './Components/ChatRooms/ChatRooms';
+import ChatRooms from './Components/ChatRoomComponents/chatrooms';
 import TaskBar from './Components/TaskBar';
 
 export default function Home(){
